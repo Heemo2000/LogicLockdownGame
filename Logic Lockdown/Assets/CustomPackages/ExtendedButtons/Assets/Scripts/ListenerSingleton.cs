e3c68af5664a36ee576fdef821d7d48ef79a3e11
Heemo2000 <hgupta01062000@gmail.com>
2024-04-08 13:44:31 +0530

    Added Extended Buttons.

---
+++ Logic Lockdown/Assets/CustomPackages/ExtendedButtons/Assets/Scripts/ListenerSingleton.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace ExtendedButtons
+{
+    public class ListenerSingleton : MonoBehaviour
+    {
+        private static IButtonsListener _instance;
+
+        [RuntimeInitializeOnLoadMethod]
+        private static void CreateInstance()
+        {
+            if (_instance == null)
+            {
+                _instance = (IButtonsListener)FindObjectOfType(typeof(ButtonsListenerMono));
+                if (_instance == null)
+                {
+                    var singletonObject = new GameObject();
+                    _instance = singletonObject.AddComponent<ButtonsListenerBasic>();
+                    singletonObject.name = typeof(ButtonsListenerBasic).ToString() + " (Singleton)";
+                }
+            }
+        }
+    }
+}
